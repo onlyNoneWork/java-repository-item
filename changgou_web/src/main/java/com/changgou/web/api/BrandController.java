e775fae62e60c17074254fb02a3302ca65a27095
onlyNoneWork <90252981+onlyNoneWork@users.noreply.github.com>
2021-09-08 18:34:50 +0800

    添加brand的查询接口

--- changgou_web/src/main/java/com/changgou/web/api/BrandController.java
+++ changgou_web/src/main/java/com/changgou/web/api/BrandController.java
@@ -4,13 +4,13 @@ import com.changgou.goods.service.BrandService;
 import com.changgou.pojo.Brand;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 /**
+ * 品牌信息
  * @author: hui.jin
  * @date: 2021/9/8 17:31
  */
@@ -22,7 +22,10 @@ public class BrandController {
 
     private BrandService brandService;
 
-
+    /**
+     * 查询所有的品牌列表
+     * @return
+     */
     public List<Brand> findBrandAll(){
         return brandService.findAll();
     }
