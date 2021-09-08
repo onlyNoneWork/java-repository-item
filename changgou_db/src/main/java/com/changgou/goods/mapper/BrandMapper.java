a9b96e5451c605c7d10b717ef57437ef3067a669
onlyNoneWork <90252981+onlyNoneWork@users.noreply.github.com>
2021-09-08 17:12:26 +0800

    基础

---
+++ changgou_db/src/main/java/com/changgou/goods/mapper/BrandMapper.java
@@ -0,0 +1,11 @@
+package com.changgou.goods.mapper;
+
+import com.changgou.goods.pojo.Brand;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * @author: jin.hui
+ * @date: 2021/9/8 14:12
+ */
+public interface BrandMapper extends Mapper<Brand> {
+}
---
+++ changgou_db/src/main/java/com/changgou/goods/pojo/Brand.java
@@ -0,0 +1,48 @@
+package com.changgou.goods.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+/**
+ * @author: hui.jin
+ * @date: 2021/9/8 14:01
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "tb_brand")
+public class Brand implements Serializable {
+
+    /**
+     * 品牌 id
+     */
+    @Id
+    private Integer id;
+
+    /**
+     * 品牌名称
+     */
+    private String name;
+
+    /**
+     * 品牌图片地址
+     */
+    private String image;
+
+    /**
+     * 品牌的首字母
+     */
+    private String letter;
+
+    /**
+     * 排序
+     */
+    private Integer seq;
+}
